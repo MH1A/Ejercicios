4f5c27880f878b267fed1681508d69ad7616d777
MH1A <113317538+MH1A@users.noreply.github.com>
2022-09-11 18:35:37 -0600

    Add files via upload

---
+++ unidad#1 semana#4 Clase#12 Ejercicio#1/src/unidad/pkg1/semana/pkg4/clase/pkg12/ejercicio/pkg1/Unidad1Semana4Clase12Ejercicio1.java
@@ -0,0 +1,17 @@
+package unidad.pkg1.semana.pkg4.clase.pkg12.ejercicio.pkg1;
+import java.util.Scanner;
+/**
+ *
+ * @author Monica
+ */
+public class Unidad1Semana4Clase12Ejercicio1 {
+
+    public static void main(String[] args) {
+        double base = 5, exponente = 2;
+        double resultado = Math.pow(base, exponente);
+        
+        System.out.println(resultado);}
+    
+    }
+    
+
