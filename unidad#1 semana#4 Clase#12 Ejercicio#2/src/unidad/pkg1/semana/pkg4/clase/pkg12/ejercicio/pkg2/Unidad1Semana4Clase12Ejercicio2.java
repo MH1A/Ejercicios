35c0b6ef11acabf8d8549cf3b793e74c5ab7f5d5
MH1A <113317538+MH1A@users.noreply.github.com>
2022-09-11 18:36:41 -0600

    Add files via upload

---
+++ unidad#1 semana#4 Clase#12 Ejercicio#2/src/unidad/pkg1/semana/pkg4/clase/pkg12/ejercicio/pkg2/Unidad1Semana4Clase12Ejercicio2.java
@@ -0,0 +1,15 @@
+package unidad.pkg1.semana.pkg4.clase.pkg12.ejercicio.pkg2;
+import java.util.Scanner; 
+/**
+ *
+ * @author Monica
+ */
+public class Unidad1Semana4Clase12Ejercicio2 {
+
+    public static void main(String[] args) {
+        double raiz =Math.sqrt(9);
+        
+        System.out.println(raiz);
+    }
+    
+}
