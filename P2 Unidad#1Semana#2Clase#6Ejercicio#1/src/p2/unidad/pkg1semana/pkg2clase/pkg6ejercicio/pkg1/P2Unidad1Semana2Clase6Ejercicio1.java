233c79c6f4fdb0a12618968008f079722920d974
MH1A <113317538+MH1A@users.noreply.github.com>
2022-09-15 10:46:22 -0600

    Add files via upload

---
+++ P2 Unidad#1Semana#2Clase#6Ejercicio#1/src/p2/unidad/pkg1semana/pkg2clase/pkg6ejercicio/pkg1/P2Unidad1Semana2Clase6Ejercicio1.java
@@ -0,0 +1,25 @@
+package p2.unidad.pkg1semana.pkg2clase.pkg6ejercicio.pkg1;
+
+/**
+ *
+ * @author Monica
+ */
+public class P2Unidad1Semana2Clase6Ejercicio1 {
+    public static void main(String[] args) {
+       String str ="dos";
+       switch (str){
+           case "uno":
+               System.out.println("uno");
+               break;
+            case "dos":
+               System.out.println("dos");
+               break;   
+            case "tres":
+               System.out.println("tres");
+               break;
+            default:
+                System.out.println("no coincide");
+       }
+    }
+    
+}
