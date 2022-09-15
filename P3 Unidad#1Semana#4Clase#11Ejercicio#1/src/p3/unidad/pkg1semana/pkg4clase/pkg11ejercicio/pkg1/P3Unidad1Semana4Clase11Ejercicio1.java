78a027929518907ea569d421b2087995687d2839
MH1A <113317538+MH1A@users.noreply.github.com>
2022-09-15 10:53:18 -0600

    Add files via upload

---
+++ P3 Unidad#1Semana#4Clase#11Ejercicio#1/src/p3/unidad/pkg1semana/pkg4clase/pkg11ejercicio/pkg1/P3Unidad1Semana4Clase11Ejercicio1.java
@@ -0,0 +1,36 @@
+package p3.unidad.pkg1semana.pkg4clase.pkg11ejercicio.pkg1;
+import java.util.Scanner;
+/**
+ *
+ * @author Monica
+ */
+public class P3Unidad1Semana4Clase11Ejercicio1 {
+    public static void main(String[] args) {
+        double peso;
+        int cont1=0, cont2=0, piezas=0;
+        Scanner entrada = new Scanner(System.in);
+        
+        do{
+            System.out.println("Ingrese 0 para salir");
+            System.out.println("Ingrese el peso: ");
+            peso = entrada.nextDouble ();
+            if (peso >= 9.8 && peso <=10.2 ) 
+            {
+                cont1= cont1 +1;
+                piezas++;
+            }
+            else if (peso > 0 && peso <= 9.8) {
+            cont2++;
+            piezas++;
+            }
+            
+        }
+        while (peso != 0);
+        System.out.println("Piezas entre 9.8 y 10.2: " +cont1);
+        System.out.println("Piezas menores 9.8: " +cont2);
+        System.out.println("Cantidad de piezas: " +piezas);
+        
+        
+    }
+    
+}
