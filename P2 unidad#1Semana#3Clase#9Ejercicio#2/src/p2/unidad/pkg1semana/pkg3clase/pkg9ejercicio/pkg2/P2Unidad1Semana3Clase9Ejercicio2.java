d375957919a3b3f35b35d73d0460b539eef3133b
MH1A <113317538+MH1A@users.noreply.github.com>
2022-09-15 10:47:38 -0600

    Add files via upload

---
+++ P2 unidad#1Semana#3Clase#9Ejercicio#2/src/p2/unidad/pkg1semana/pkg3clase/pkg9ejercicio/pkg2/P2Unidad1Semana3Clase9Ejercicio2.java
@@ -0,0 +1,18 @@
+package p2.unidad.pkg1semana.pkg3clase.pkg9ejercicio.pkg2;
+
+/**
+ *
+ * @author Monica
+ */
+public class P2Unidad1Semana3Clase9Ejercicio2 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++) {
+            System.out.println("********* Tabla del "+i+" *************");
+            for (int j=1; j<=10; j++){ 
+                System.out.println(i+" multiplicando por "+j+" es igual a " +i*j);
+                
+            }
+        }
+    }
+    
+}
