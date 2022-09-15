c1272edba4c818d424ea67b7ee1c272fc30e5b2b
MH1A <113317538+MH1A@users.noreply.github.com>
2022-09-15 10:48:14 -0600

    Add files via upload

---
+++ P2 unidad#1Semana#4Clase#12Ejercicio#2/src/p2/unidad/pkg1semana/pkg4clase/pkg12ejercicio/pkg2/P2Unidad1Semana4Clase12Ejercicio2.java
@@ -0,0 +1,16 @@
+package p2.unidad.pkg1semana.pkg4clase.pkg12ejercicio.pkg2;
+
+/**
+ *
+ * @author Monica
+ */
+public class P2Unidad1Semana4Clase12Ejercicio2 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i=100; i<=200; i++) {
+            suma = suma + i;
+        }
+        System.out.println("La suma es: " + suma);
+    }
+    
+}
